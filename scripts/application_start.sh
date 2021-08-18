0077646ba0aff50c7be41131442b5111148d6ff4
andrew-patzer <80986998+andrew-patzer@users.noreply.github.com>
2021-08-18 15:35:25 -0500

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -7,7 +7,7 @@ cd /home/ec2-user/express-app
 #Add npm and node to path
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # loads nvm
-[ -s "$NVM_DIR/bash_completion"] && \. "$NVM_DIR/bash_completion" # loads nvm bash_completion
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # loads nvm bash_completion
 
 #install node modules
 npm install
